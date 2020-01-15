f156ee00778cb220dfa118f5c72713817a35f597
MosT98 <andreihalauca@yahoo.com>
2020-01-15 12:15:26 +0200

    added graphics

--- Board_Representation.py
+++ Board_Representation.py
@@ -1,12 +1,13 @@
+import sys
 import numpy as np
 import pandas as pd
+from Graphic_Representation import *
+
 
 pd.set_option('display.expand_frame_repr', False)
-
 ROWS = 8
 COLUMNS = 8
 DIRECTIONS = [[-1, -1], [0, -1], [1, -1], [-1, 0], [1, 0], [-1, 1], [0, 1], [1, 1]]  # NV, N, NE, E, V, SV, S, SE
-
 
 def create_board():
     board = np.zeros((ROWS, COLUMNS), dtype=int)
@@ -128,34 +129,46 @@ def make_move(board, row, column, piece):
         return False
 
     board[row][column] = piece
+
     for r, c in pieces_to_flip:
         board[r][c] = piece
+
+    drawing_pieces(board, piece)
     return True
 
 
 if __name__ == '__main__':
+    pg.init()
     board = create_board()
-    # b2 = board_with_moves(board, 1)
-    # show_board(b2)
-    turn = 'p1'
-    while True:
-        if turn == 'p1':
-            show_board(board)
-            move = player_move(board, 1)
-            make_move(board, move[0], move[1], 1)
-
-            if get_moves(board, 2) == []:
-                break
-            else:
-                turn = 'p2'
-        elif turn == 'p2':
-            show_board(board)
-            move = player_move(board, 2)
-            make_move(board, move[0], move[1], 2)
-
-            if get_moves(board, 1) == []:
-                break
-            else:
-                turn = 'p1'
-
+    screen = pg.display.set_mode(BOARD_SIZE)
+    draw_board(board)
+    pg.display.update()
     show_board(board)
+    game_over = False
+    turn = 0
+    while not game_over:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                sys.exit()
+
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if turn == 0:
+                    print("PLAYER 1")
+                    position = get_mouse_position()
+                    make_move(board, position[0], position[1], 1)
+                    if get_moves(board, 2) == []:
+                        game_over = True
+                        break
+                    else:
+                        turn += 1
+                elif turn == 1:
+                    print("PLAYER 2")
+                    position = get_mouse_position()
+                    make_move(board, position[0], position[1], 2)
+                    if get_moves(board, 1) == []:
+                        game_over = True
+                        break
+                    else:
+                        turn -= 1
+        if game_over == True:
+            pg.time.wait(3000)
---
+++ Graphic_Representation.py
@@ -0,0 +1,57 @@
+import pygame as pg
+ROWS = 8
+COLUMNS = 8
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+BACKGROUND = (0, 204, 153)
+WHITE = (255, 255, 255)
+
+PIECE_SIZE = 100
+BOARD_WIDTH = PIECE_SIZE * COLUMNS
+BOARD_HEIGHT = PIECE_SIZE * ROWS
+BOARD_SIZE = (BOARD_WIDTH, BOARD_HEIGHT)
+RADIUS = int(PIECE_SIZE / 2 - 5)
+screen = pg.display.set_mode(BOARD_SIZE)
+pg.display.set_caption('Othello')
+
+
+def draw_board(board):
+    for c in range(COLUMNS):
+        for r in range(ROWS):
+            pg.draw.rect(screen, BACKGROUND, (c * PIECE_SIZE, r * PIECE_SIZE, PIECE_SIZE, PIECE_SIZE))
+            pg.draw.circle(screen, BACKGROUND,
+                           (int(c * PIECE_SIZE + PIECE_SIZE / 2), int(r * PIECE_SIZE + PIECE_SIZE / 2)),
+                           RADIUS)
+
+    for c in range(COLUMNS):
+        for r in range(ROWS):
+            if board[r][c] == 2:
+                pg.draw.circle(screen, BLACK, (int(c * PIECE_SIZE + PIECE_SIZE / 2),
+                                               BOARD_HEIGHT - int(r * PIECE_SIZE + PIECE_SIZE / 2)), RADIUS)
+            elif board[r][c] == 1:
+                pg.draw.circle(screen, WHITE, (int(c * PIECE_SIZE + PIECE_SIZE / 2),
+                                               BOARD_HEIGHT - int(r * PIECE_SIZE + PIECE_SIZE / 2)), RADIUS)
+    pg.display.update()
+
+
+def get_mouse_position():
+    (mouse_x, mouse_y) = pg.mouse.get_pos()
+    position = (mouse_x // PIECE_SIZE), (mouse_y // PIECE_SIZE)
+    position = (position[1], position[0])
+    return position
+
+
+def drawing_pieces(board, piece):
+    if piece == 1:
+        COLOR = BLACK
+    elif piece == 2:
+        COLOR = WHITE
+
+    for row in range(8):
+        for column in range(8):
+            if board[row][column] == piece:
+                pg.draw.circle(screen, COLOR, (
+                    column * 100 + PIECE_SIZE // 2, row * 100 + PIECE_SIZE // 2), RADIUS)
+                pg.display.update()
